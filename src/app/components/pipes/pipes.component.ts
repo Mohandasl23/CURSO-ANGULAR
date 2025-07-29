188969e1819b72d5c1a76580bf96fd1b84fe5323
Mohandasl23 <mohandasgomes@yahoo.com.br>
2025-07-29 06:21:14 -0300

    Crie a pasta PIPES e aprendi a usar o pipe

---
+++ src/app/components/pipes/pipes.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pipes',
+  standalone: false,
+  templateUrl: './pipes.component.html',
+  styleUrl: './pipes.component.css'
+})
+export class PipesComponent implements OnInit {
+  someText = 'TESTANDO O PIPE OPERATOR';
+
+  today = new Date();
+
+  constructor() { }
+
+  ngOnInit(): void {      
+  }
+
+}
